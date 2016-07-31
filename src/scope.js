50e7b9e8f6054ea4b4ffdc7c186eb274154e2911
Manny Karyampudi <mrkaryam@gmail.com>
2016-07-31 16:59:59 -0700

    Chapter 2 - Scope Methods -  (p. 79)

--- src/scope.js
+++ src/scope.js
@@ -2,91 +2,105 @@
 
 var _ = require('lodash');
 
-function initWatchVal() { }
-
-function Scope() {
-	// $$ prefix signifies that this variable should be considered private in the Angular framework.
-	this.$$watchers = [];
-	this.$$lastDirtyWatch = null;
-	this.$$asyncQueue = [];
+function initWatchVal() {
 }
 
-Scope.prototype.$watch = function(watchFn, listenerFn, valueEq) {
-	var self = this;
-	var watcher = {
-		watchFn: watchFn,
-		listenerFn: listenerFn || function() {},
-		valueEq: !!valueEq,
-		last: initWatchVal
-	};
-	self.$$watchers.unshift(watcher);
-	this.$$lastDirtyWatch = null;
-	return function() {
-		var index = self.$$watchers.indexOf(watcher);
-		if (index >= 0) {
-			self.$$watchers.splice(index, 1);
-			self.$$lastDirtyWatch = null;
-		}
-	};
+function Scope() {
+  // $$ prefix signifies that this variable should be considered private in the Angular framework.
+  this.$$watchers = [];
+  this.$$lastDirtyWatch = null;
+  this.$$asyncQueue = [];
+  this.$$applyAsyncQueue = [];
+  this.$$applyAsyncId = null;
+  this.$$phase = null;
+}
+
+Scope.prototype.$watch = function (watchFn, listenerFn, valueEq) {
+  var self = this;
+  var watcher = {
+    watchFn: watchFn,
+    listenerFn: listenerFn || function () {
+    },
+    valueEq: !!valueEq,
+    last: initWatchVal
+  };
+  self.$$watchers.unshift(watcher);
+  this.$$lastDirtyWatch = null;
+  return function () {
+    var index = self.$$watchers.indexOf(watcher);
+    if (index >= 0) {
+      self.$$watchers.splice(index, 1);
+      self.$$lastDirtyWatch = null;
+    }
+  };
 };
 
 // Call listener functions on all registered watchers
 // Check if values specified by watch functions have actually changed, and _only then_ call
 // the respective listener functions ==> "DIRTY CHECKING"
-Scope.prototype.$$digestOnce = function() {
-	var self = this;
-	var newValue, oldValue, dirty;
-	_.forEachRight(this.$$watchers, function(watcher) {
-		try {
-			if (watcher) {
-				newValue = watcher.watchFn(self);
-				oldValue = watcher.last;
-				if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
-					self.$$lastDirtyWatch = watcher;
-					watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
-					watcher.listenerFn(newValue, 
-						(oldValue === initWatchVal ? newValue : oldValue), 
-						self);
-					dirty = true;
-				} else if (self.$$lastDirtyWatch === watcher) {
-					return false;
-				}
-			}
-		} catch (e) {
-			console.error(e);
-		}
-	});
-	return dirty;
+Scope.prototype.$$digestOnce = function () {
+  var self = this;
+  var newValue, oldValue, dirty;
+  _.forEachRight(this.$$watchers, function (watcher) {
+    try {
+      if (watcher) {
+        newValue = watcher.watchFn(self);
+        oldValue = watcher.last;
+        if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
+          self.$$lastDirtyWatch = watcher;
+          watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
+          watcher.listenerFn(newValue,
+            (oldValue === initWatchVal ? newValue : oldValue),
+            self);
+          dirty = true;
+        } else if (self.$$lastDirtyWatch === watcher) {
+          return false;
+        }
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  });
+  return dirty;
 };
 
-Scope.prototype.$digest = function() {
-	var ttl = 10;
-	var dirty;
-	this.$$lastDirtyWatch = null;
-	do {
-		while (this.$$asyncQueue.length) {
-			var asyncTask = this.$$asyncQueue.shift();
-			asyncTask.scope.$eval(asyncTask.expression);
-		}
-		dirty = this.$$digestOnce();
-		if (dirty && !(ttl--)) {
-			throw '10 digest iterations reached';
-		}
-	} while (dirty);
+Scope.prototype.$digest = function () {
+  var ttl = 10;
+  var dirty;
+  this.$$lastDirtyWatch = null;
+  this.$beginPhase('$digest');
+
+  if (this.$$applyAsyncId) {
+    clearTimeout(this.$$applyAsyncId);
+    this.$$flushApplyAsync();
+  }
+
+  do {
+    while (this.$$asyncQueue.length) {
+      var asyncTask = this.$$asyncQueue.shift();
+      asyncTask.scope.$eval(asyncTask.expression);
+    }
+    dirty = this.$$digestOnce();
+    if ((dirty || this.$$asyncQueue.length) && !(ttl--)) {
+      this.$clearPhase();
+      throw '10 digest iterations reached';
+    }
+  } while (dirty || this.$$asyncQueue.length);
+  this.$clearPhase();
 };
 
-Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq) {
+Scope.prototype.$$areEqual = function (newValue, oldValue, valueEq) {
   if (valueEq) {
     return _.isEqual(newValue, oldValue);
   } else {
     return newValue === oldValue ||
-    	(typeof newValue === 'number' && typeof oldValue === 'number' &&
-    		isNaN(newValue) && isNaN(oldValue));
+      (typeof newValue === 'number' && typeof oldValue === 'number' &&
+      isNaN(newValue) && isNaN(oldValue));
   }
 };
 
-Scope.prototype.$eval = function(expr, locals) {
-	return expr(this, locals);
+Scope.prototype.$eval = function (expr, locals) {
+  return expr(this, locals);
 };
 
 
@@ -94,12 +108,14 @@ Scope.prototype.$eval = function(expr, locals) {
 // If we wrap this code in $apply, then we can be sure that any watches on the scope will
 // pick up on those changes. This is the meaning of "integrating code to the Angular lifecycle
 // using $apply"
-Scope.prototype.$apply = function(expr) {
-	try {
-		return this.$eval(expr);
-	} finally {
-		this.$digest();
-	}
+Scope.prototype.$apply = function (expr) {
+  try {
+    this.$beginPhase('$apply');
+    return this.$eval(expr);
+  } finally {
+    this.$clearPhase();
+    this.$digest();
+  }
 };
 
 // For deferred execution - execute at some point in the future when the current execution
@@ -110,8 +126,46 @@ Scope.prototype.$apply = function(expr) {
 // other work before it gets to your timeout. However, with $evalAsync, it is guaranteed to run
 // before the browser decides to do anything else.
 // Use $evalAsync to prevent unnecessary rendering
-Scope.prototype.$evalAsync = function(expr) {
-	this.$$asyncQueue.push({scope: this, expression: expr});
+Scope.prototype.$evalAsync = function (expr) {
+  var self = this;
+  if (!self.$$phase && !self.$$asyncQueue.length) {
+    setTimeout(function () {
+      if (self.$$asyncQueue.length) {
+        self.$digest();
+      }
+    }, 0);
+  }
+  this.$$asyncQueue.push({scope: this, expression: expr});
+};
+
+Scope.prototype.$beginPhase = function (phase) {
+  if (this.$$phase) {
+    throw this.$$phase + ' already in progress.';
+  }
+  this.$$phase = phase;
+};
+
+Scope.prototype.$clearPhase = function () {
+  this.$$phase = null;
+};
+
+Scope.prototype.$$flushApplyAsync = function () {
+  while (this.$$applyAsyncQueue.length) {
+    this.$$applyAsyncQueue.shift()(); // double ()() - shift function and then invoke it.
+  }
+  this.$$applyAsyncId = null;
+};
+
+Scope.prototype.$applyAsync = function (expr) {
+  var self = this;
+  self.$$applyAsyncQueue.push(function () {
+    self.$eval(expr);
+  });
+  if (self.$$applyAsyncId === null) {
+    self.$$applyAsyncId = setTimeout(function () {
+      self.$apply(_.bind(self.$$flushApplyAsync, self));
+    }, 0);
+  }
 };
 
 module.exports = Scope;
