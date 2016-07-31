5201a7e7ea3b9697e867bd498e8ee1eaff75cb93
Manny Karyampudi <mrkaryam@gmail.com>
2016-07-30 19:48:10 -0700

    Chapter 1 - Scopes and Dirty Checking - Value-Based Dirty Checking (p. 51)

--- src/scope.js
+++ src/scope.js
@@ -10,10 +10,11 @@ function Scope() {
 	this.$$lastDirtyWatch = null;
 }
 
-Scope.prototype.$watch = function(watchFn, listenerFn) {
+Scope.prototype.$watch = function(watchFn, listenerFn, valueEq) {
 	var watcher = {
 		watchFn: watchFn,
 		listenerFn: listenerFn || function() {},
+		valueEq: !!valueEq,
 		last: initWatchVal
 	};
 	this.$$watchers.push(watcher);
@@ -29,9 +30,9 @@ Scope.prototype.$$digestOnce = function() {
 	_.forEach(this.$$watchers, function(watcher) {
 		newValue = watcher.watchFn(self);
 		oldValue = watcher.last;
-		if (newValue != oldValue) {
+		if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
 			self.$$lastDirtyWatch = watcher;
-			watcher.last = newValue;
+			watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
 			watcher.listenerFn(newValue, 
 				(oldValue === initWatchVal ? newValue : oldValue), 
 				self);
@@ -53,6 +54,14 @@ Scope.prototype.$digest = function() {
 			throw '10 digest iterations reached';
 		}
 	} while (dirty);
+};
+
+Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq) {
+  if (valueEq) {
+    return _.isEqual(newValue, oldValue);
+  } else {
+    return newValue === oldValue;
+  }
 };
 
 module.exports = Scope;
